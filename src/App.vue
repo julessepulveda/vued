4a0c26ac924f70b0d8c78acf813a1bea0a8a3242
Jules Sepulveda <jules.sepulveda@provisionsgroup.com>
2020-12-07 19:03:55 -0600

    todo list edit todo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList'  
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoList
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
 </script>
 
 <style>
